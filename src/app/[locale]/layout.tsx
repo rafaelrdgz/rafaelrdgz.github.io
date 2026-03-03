f83d0713dbbbcebb0e6ecded64400bc70100831a
Rafael Rodriguez <rafaelrdgz2003@gmail.com>
2026-03-03 13:22:41 -0500

    Add favicon and icons for the public directory
    - Added favicon.ico to enhance the website's branding.
    - Introduced icon1.png and icon2.png for improved visual representation.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -33,6 +33,14 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     category: 'technology',
     metadataBase: new URL(baseUrl),
     manifest: '/manifest.json',
+    icons: {
+      icon: [
+        { url: '/favicon.ico', sizes: '16x16', type: 'image/x-icon' },
+        { url: '/icon1.png', sizes: '16x16', type: 'image/png' },
+        { url: '/icon2.png', sizes: '32x32', type: 'image/png' },
+      ],
+      apple: [{ url: '/apple-icon.png', sizes: '128x128', type: 'image/png' }],
+    },
     authors: [{ name: 'Rafael Rodriguez' }],
     keywords: [
       'Rafael Rodriguez',
